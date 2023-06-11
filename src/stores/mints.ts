8be5e78aece6acfa7a219c10012183b0901174e8
mrq <mrq@mrq.cz>
2023-06-11 07:52:58 +0200

    3338

--- src/stores/mints.ts
+++ src/stores/mints.ts
@@ -18,7 +18,7 @@ export const useMintsStore = defineStore("mints", {
       activeProofs: useLocalStorage("cashu.activeProofs", [] as Proof[]),
       keys: useLocalStorage("cashu.keys", {} as MintKeys),
       keysets: useLocalStorage("cashu.keysets", [] as string[]),
-      mintToAdd: "http://ethprague.play.hydration.cloud:8883",
+      mintToAdd: "http://ethprague.play.hydration.cloud:3338",
       mints: useLocalStorage("cashu.mints", [] as Mint[]),
       proofs: useLocalStorage("cashu.proofs", [] as Proof[]),
       showAddMintDialog: false,
